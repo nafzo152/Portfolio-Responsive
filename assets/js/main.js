6479241fe0173343c442fa5f1e901a45c132790d
Cargo <ahmednafisbd15@gmail.com>
2024-07-22 10:45:11 +0800

    nav show and nav hide in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,25 +3,20 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
+/*=============== MENU SHOW ===============*/
+/* Validate if navToggle Exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
-      /*Validate if constant Exists*/
-
-      if(navToggle)
-      {
-        navToggle.addEventListener('click', () =>{
-            navMenu.classList.add('show-menu')
-        })
-      }
-
-
- /*=============== MENU HIDDEN ===============*/
-
-            /*Validate if constant Exists*/
-
-if(navClose){
-    navClose.addEventListener('click', () =>{
+/*=============== MENU HIDDEN ===============*/
+/* Validate if navClose Exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
-    })     
+    })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
